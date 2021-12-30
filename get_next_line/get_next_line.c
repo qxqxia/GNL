e34605f34acb491c093d3f2eb31693604c681b31
Qin XIA <qxia@e1r11p8.42.fr>
2021-12-30 18:09:42 +0100

    GNL

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qxia <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/27 15:53:16 by qxia              #+#    #+#             */
+/*   Updated: 2021/12/29 18:22:05 by qxia             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*line1(const char *str)
+{
+	size_t		i;
+	char		*s;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+		i++;
+	s = (char *)malloc(sizeof(char) * i + 1);
+	if (!s)
+		return (NULL);
+	while (str[i])
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
+
+char	*rest_lines(const char *lines)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	if (!lines)
+		return (0);
+	while (lines[i])
+		i++;
+	str = (char *)malloc(sizeof(char) * ft_strlen(lines) - i + 1);
+	if (!str)
+		return (NULL);
+	while (lines[i])
+	{
+		str[i] = line[i]
+	}
+}
+
+
+char	*get_next_line(int fd)
+{
+	char	*buffer;
+	char	*line;
+	static char	*rest;
+
+
+}
---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qxia <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/27 15:53:58 by qxia              #+#    #+#             */
+/*   Updated: 2021/12/29 18:22:24 by qxia             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i ++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*s;
+	size_t	i;
+	size_t	j;
+
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	if (!s1 && !s2)
+		return (NULL);
+	if (s1 && !s2)
+		return 
+
+}	
+
